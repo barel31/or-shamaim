12ab4e5c098ae9baac2c739b4d54851108dead93
barel31 <barel123133@gmail.com>
2022-06-07 14:58:42 +0300

    Fxed burger menu dont open on very small screens

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -31,7 +31,7 @@ export default function NavBar({ buttons, routes }) {
                 <HiMenuAlt4 onClick={() => setToggle(true)} />
 
                 {toggle && (
-                    <motion.div whileInView={{ x: [300, 0] }} transition={{ duration: 0.85, ease: 'easeOut' }}>
+                    <motion.div whileInView={{ x: [100, 0] }} transition={{ duration: 0.85, ease: 'easeInOut' }}>
                         <HiX onClick={() => setToggle(false)} />
                         <ul>
                             {buttons.map((v, i) => (
